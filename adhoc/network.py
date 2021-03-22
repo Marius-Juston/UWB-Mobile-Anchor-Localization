1a97602ae7fc24efe7336dd97b636be571fdca7c
Marius-Juston <Marius.juston@hotmail.fr>
2021-03-21 22:12:37 -0400

    implementated full network creation

--- adhoc/network.py
+++ adhoc/network.py
@@ -11,9 +11,11 @@ class NodeEnum(Enum):
     MobileNode = {'id': 1, 'color': (0, 1, 0), 'p': 1}
 
 
-    def __init__(self, N=10) -> None:
+class RandomNetwork:
+    def __init__(self, N=10, start_range=((-10, 10), (-10, 10))) -> None:
         super().__init__()
 
+        self.start_range = start_range
         self.N = N
         self.network = None
 
@@ -33,10 +35,31 @@ class NodeEnum(Enum):
             p=p
         )
 
+        node_pose = np.vstack([
+            np.random.uniform(self.start_range[0][0], self.start_range[0][1], self.N),
+            np.random.uniform(self.start_range[1][0], self.start_range[1][1], self.N)
+        ])
 
+        anchor_id = 0
 
-        print(node_choices)
+        tag_id = 0
+
+        self.network = []
+
+        for index, node in enumerate(node_choices):
+            actual_pose = node_pose[:, index]
+
+            if node is NodeEnum.StationaryNode:
+                self.network.append(StationaryNode(anchor_id, actual_pose))
+                anchor_id += 1
+            elif node is NodeEnum.MobileNode:
+                self.network.append(MobileNode(anchor_id, tag_id, tag_id + 1, actual_pose))
+
+                anchor_id += 1
+                tag_id += 2
+
+            print(node, len(self.network), actual_pose)
 
 
 if __name__ == '__main__':
-    
+    node_network = RandomNetwork()
